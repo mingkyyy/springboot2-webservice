f7c6b91ed85abae56c17b24a398a260d8991d4d3
mingkyy <alsrus1227@naver.com>
2021-12-11 11:37:45 +0900

    머스테치로 화면 구성하기 - 수정, 삭제 기능

--- src/main/java/com/mingkyy/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/mingkyy/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.mingkyy.book.springboot.service.posts;
 
 import com.mingkyy.book.springboot.domain.posts.PostRepository;
 import com.mingkyy.book.springboot.domain.posts.Posts;
+import com.mingkyy.book.springboot.web.dto.PostsListResponseDto;
 import com.mingkyy.book.springboot.web.dto.PostsResponseDto;
 import com.mingkyy.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 @RequiredArgsConstructor
@@ -33,5 +37,20 @@ public class PostsService {
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
+    }
+
+   @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+   }
+
+   @Transactional
+    public void delete(Long id){
+    Posts posts = postRepository.findById(id)
+            .orElseThrow(()->new
+                    IllegalArgumentException("해당 게시물이 없습니다. id = "+id));
+    postRepository.delete(posts);
     }
 }
